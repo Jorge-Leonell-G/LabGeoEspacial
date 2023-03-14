07e0c896af6b3ef26ff0f8211e32f0e92ce4ec9b
ArturoParra <72054956+ArturoParra@users.noreply.github.com>
2023-03-14 16:00:23 -0600

    Delete JS directory

--- JS/boton.js
+++
@@ -1,8 +0,0 @@
-//botón de menú responsive
-
-const toggleButton = document.getElementsByClassName('toggle-button')[0]
-const listcontainer = document.getElementsByClassName('list-container')[0]
-
-toggleButton.addEventListener('click', () => {
-    listcontainer.classList.toggle('active')
-})
